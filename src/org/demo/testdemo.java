043e28e1a63ba396199f1dbab568a1bec51b4127
PMS1523 <pmssudhagar07@gmail.com>
2021-08-30 18:15:14 +0530

    hai

---
+++ src/org/demo/testdemo.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class testdemo {
+private void suma() {
+	System.out.println("haj");
+	System.out.println("jdhif");
+}
+public static void main(String[] args) {
+	testdemo ts= new testdemo();
+	ts.suma();
+	System.out.println( "hugdi8");
+	System.out.println("gujgdiuf");
+	
+		
+	}
+}
+
